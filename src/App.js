f6550198fd3992c0ea4754ca926e5454d13d7f8b
lakshay-dutt <lakshay91dutt@gmail.com>
2021-05-09 17:29:12 +0530

    image grid added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import * as Back from "./assets/Back.png";
+import * as Search from "./assets/search.png";
+import page from "./api/CONTENTLISTINGPAGE-PAGE1.json";
+import CardsGrid from "./components/CardsGrid";
+import useScroll from "./hooks/scrollHook";
 
+function Header() {
+  return (
+    <header className="flex justify-between h-10">
+      <img className="h-6 w-6 cursor-pointer" src={Back.default} alt="back" />
+      <span className="w-full ml-3 text-white">Romantic Comedy</span>
+      <img className="h-6 w-6 ml-2 cursor-pointer" src={Search.default} alt="search" />
+    </header>
+  );
+}
 function App() {
-  return <div className="App"></div>;
+  const content = (((page || {}).page || {})["content-items"] || {}).content || [];
+  return (
+    <div className="App">
+      <main className="mx-auto my-10 mweb-container" style={{ width: "425px" }}>
+        <div className="mweb-container wrapper px-4 pt-4 relative border-2 shadow-2xl border-gray-100 bg-black h-screen" style={{ borderRadius: "35px" }}>
+          <Header />
+          {content.length > 0 && (
+            <div className="mt-3 scroll-effect">
+              <CardsGrid contentItems={content} />
+            </div>
+          )}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CardsGrid.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import LazyLoadImage from "./LazyLoadImage";
+import Placholder from "../assets/placeholder_for_missing_posters.png";
+const Card = ({ item }) => {
+  return item["poster-image"] && item.name ? (
+    <div className="w-1/3 px-2 mb-4 h-full">
+      <LazyLoadImage
+        className="h-full rounded-lg  mx-auto object-cover shadow-2xl"
+        src={require(`../assets/${item["poster-image"]}`).default}
+        alt={item.name}
+        placeholderImg={Placholder}
+      />
+      {item.name}
+    </div>
+  ) : (
+    <React.Fragment />
+  );
+};
+const CardsGrid = ({ contentItems }) => {
+  const [items, setItems] = useState(18);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <React.Fragment>
+      <div className="flex flex-wrap -mx-2 mb-8 max-h-full">
+        {[...contentItems].slice(0, items).map((item, i) => (
+          <React.Fragment key={i}>
+            <Card item={item} />
+          </React.Fragment>
+        ))}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default CardsGrid;
